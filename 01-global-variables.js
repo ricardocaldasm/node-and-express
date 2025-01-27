91b7ecd4ab43791cd584588b9aa3e72e5303b4e1
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2025-01-27 09:48:22 -0300

    day1

---
+++ 01-global-variables.js
@@ -0,0 +1,14 @@
+// GLOBALS - NO WINDOW!!
+
+// __dirname    - path do current directory
+// __filename   - file name
+// require      - function do use modules (CommonJS)
+// module       - info about env where the program is being executed
+
+console.log(__dirname);
+console.log("------");
+console.log(__filename);
+console.log("------");
+console.log(require);
+console.log("------");
+console.log(module);
