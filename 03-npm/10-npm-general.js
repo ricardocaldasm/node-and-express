d420ec7aaa961633f69055bda52ecf27e5111d91
ricardocaldasm <79159591+ricardocaldasm@users.noreply.github.com>
2025-01-31 11:04:36 -0300

    day3

--- 03-npm/10-npm-general.js
+++ 03-npm/10-npm-general.js
@@ -8,6 +8,8 @@
 // npm install -g <packageName>
 // sudo npm install -g <packageName> (mac)
 
+// npm install -d - Just used for the developer - like nodemon
+
 // package.json - manifest file (store important info about project/package)
 // manual approach (create package.json in the root, create properties etc)
 // npm init (step by step, press enter to skip)
@@ -18,3 +20,8 @@ import _ from "lodash";
 const items = [1, [2, [3, [4]]]];
 const newItems = _.flattenDeep(items);
 console.log(newItems);
+
+// usage of npm start - configure package.json "scripts": {"start": "node 10-npm-general.js"}
+// usage of npm run dev - CUSTOM SCRIPT: "dev": "nodemon 10-npm-general.js" -
+
+// npm uninstall lodash
